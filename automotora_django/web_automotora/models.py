d358153a806052ad059f17da06d157dae6cca71f
DuocCosas <jor.videla@profesor.duoc.cl>
2024-05-24 18:07:50 -0400

    fix uuid

--- automotora_django/web_automotora/models.py
+++ automotora_django/web_automotora/models.py
@@ -1,9 +1,10 @@
+import uuid
 from django.db import models
 
 # Create your models here.
 
 class Auto(models.Model):
-    id = models.UUIDField(primary_key=True)
+    id = models.UUIDField(primary_key=True,default=uuid.uuidv4,editable=False)
     marca = models.CharField(max_length=255,null=False)
     modelo = models.CharField(max_length=255,null=False)
     año = models.IntegerField(null=False)
