204e362adec8ba96bcef37345a0b2be24419e516
DuocCosas <jor.videla@profesor.duoc.cl>
2024-05-24 21:45:34 -0400

    cambios

--- automotora_django/web_automotora/models.py
+++ automotora_django/web_automotora/models.py
@@ -1,11 +1,18 @@
+import datetime
 import uuid
 from django.db import models
 
 # Create your models here.
 
 class Auto(models.Model):
-    id = models.UUIDField(primary_key=True,default=uuid.uuidv4,editable=False)
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
     marca = models.CharField(max_length=255,null=False)
     modelo = models.CharField(max_length=255,null=False)
     año = models.IntegerField(null=False)
     precio = models.IntegerField(null=False)
+    fecha = models.DateTimeField(null=False,default=datetime.date.today(),editable=False)
+    comprado = models.BooleanField(null=False, default= False)
+    imagen = models.ImageField(upload_to='static/subidas',null=True)
+
+    def __str__(self):
+        return str(self.año)+' '+self.marca+' '+self.modelo
--- automotora_django/web_automotora/views.py
+++ automotora_django/web_automotora/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from .models import Auto
 
 # Create your views here.
 
 def inicio(request):
-    return render(request,'inicio.html')
+
+    autos = Auto.objects.filter(comprado = False)
+
+    contexto = {
+        "autos":autos
+    }
+
+    return render(request,'inicio.html',contexto)
 
 def info(request):
     return render(request,'info.html')
