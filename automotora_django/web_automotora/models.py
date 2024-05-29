f51af5b7fe56627d9e76dc84fd141f7b3c84a338
Jorge Videla <jorge.videla.v@gmail.com>
2024-05-29 12:27:18 -0400

    arreglos pre clases

--- automotora_django/web_automotora/models.py
+++ automotora_django/web_automotora/models.py
@@ -1,5 +1,6 @@
 import datetime
 import uuid
+from django import utils
 from django.db import models
 
 # Create your models here.
@@ -10,9 +11,9 @@ class Auto(models.Model):
     modelo = models.CharField(max_length=255,null=False)
     año = models.IntegerField(null=False)
     precio = models.IntegerField(null=False)
-    fecha = models.DateTimeField(null=False,default=datetime.date.today(),editable=False)
+    fecha = models.DateTimeField(null=False,default=utils.timezone.now,editable=False)
     comprado = models.BooleanField(null=False, default= False)
-    imagen = models.ImageField(upload_to='static/subidas',null=True)
+    imagen = models.ImageField(upload_to='autos/',null=True)
 
     def __str__(self):
         return str(self.año)+' '+self.marca+' '+self.modelo
