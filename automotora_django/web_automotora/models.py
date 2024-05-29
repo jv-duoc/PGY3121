d4def261cd1673b64f32e233e9192e9a9f6de608
Jorge Videla <jorge.videla.v@gmail.com>
2024-05-29 12:07:41 -0400

    a

--- automotora_django/web_automotora/models.py
+++ automotora_django/web_automotora/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Auto(models.Model):
-    id = models.UUIDField(primary_key=True)
+    id = models.UUIDField(primary_key=True,editable=False)
     marca = models.CharField(max_length=255,null=False)
     modelo = models.CharField(max_length=255,null=False)
     año = models.IntegerField(null=False)
