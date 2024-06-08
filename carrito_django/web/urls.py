83dca3804a7f6a42e5b61195b2af646dd08724f8
DuocCosas <jor.videla@profesor.duoc.cl>
2024-06-08 09:38:38 -0400

    login

--- carrito_django/web/urls.py
+++ carrito_django/web/urls.py
@@ -4,4 +4,7 @@ from web import views
 
 urlpatterns = [
     path('', views.inicio,name='inicio'),
+    path('login',views.login_vista,name='login'),
+    path('logout',views.salir,name="logout"),
+    path('mi-cuenta',views.cuenta,name='cuenta')
 ]
--- carrito_django/web/views.py
+++ carrito_django/web/views.py
@@ -1,6 +1,43 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.contrib.auth import login,authenticate,logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def inicio(request):
     return render(request,'inicio.html');
+
+@login_required(login_url='/login')
+def cuenta(request):
+    return render(request,'cuenta.html')
+
+
+def salir(request):
+    if request.user.is_authenticated:
+        logout(request)
+    return redirect('inicio');
+
+def login_vista(request):
+
+    error = ''
+
+    if request.method == 'POST':
+        usuario = request.POST.get('usuario')
+        contra = request.POST.get('password')
+
+        if len(usuario) < 1:
+            error += 'Debe ingresar nombre de usuario.'
+
+        if len(contra) < 1:
+            error += 'Debe ingresar contraseña.'
+
+
+        user = authenticate(username = usuario,password = contra)
+
+        if user is None:
+            error += 'Las credenciales no son validas.'
+        else: 
+            login(request,user)
+            return redirect('inicio')
+
+
+    return render(request,'login.html',{"error":error})
