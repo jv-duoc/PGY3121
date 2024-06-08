5eed882ea128c5dc7ac469a31ec47f51d64f9acf
DuocCosas <jor.videla@profesor.duoc.cl>
2024-06-07 23:04:41 -0400

    p

--- carrito_django/web/models.py
+++ carrito_django/web/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Producto(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    nombre = models.CharField(max_length=255,null=False)
+    imagen = models.ImageField(upload_to='productos',null=False)
+    precio = models.IntegerField(null=False)
